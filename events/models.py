1193be8b011727f386b2e62854f7c0c28c32f5a5
Artyom Rudenko <arudenko@kozhin.dev>
2026-09-15 18:20:59 +0700

    feat: add Venue model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+class Venue(models.Model):
+    """Место проведения мероприятия"""
+
+    name = models.CharField(max_length=255, verbose_name="Название места")
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(Decimal("-90.000000")), MaxValueValidator(Decimal("90.000000"))],
+        verbose_name="Широта"
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(Decimal("-180.000000")), MaxValueValidator(Decimal("180.000000"))],
+        verbose_name="Долгота"
+    )
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return f"{self.name}"
